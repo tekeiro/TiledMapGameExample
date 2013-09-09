6f93583f3090edd115810f626ca4b58f83587961
tekeiro <tekeiro@gmail.com>
2013-09-09 17:53:54 +0200

    [code][upload] Initial upload of LibGDX projects

---
+++ TiledMapGameExample-desktop/src/com/keirobm/tiledmapexample/Main.java
@@ -0,0 +1,16 @@
+package com.keirobm.tiledmapexample;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "TiledMapGameExample";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new TiledMapExampleMain(), cfg);
+	}
+}
